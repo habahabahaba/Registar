3384e8892275d5596bd9f2f7fcfc41ec7c3b6211
Alex S <habahabahaba@outlook.com>
2024-09-13 04:16:00 +0400

    Add router and router-provider to App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,43 @@
 // Redux RTK:
 // Store:
 // React Router:
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // React:
 // Context:
+// Pages:
+import Root from './Pages/Root';
+import HomePage from './Pages/HomePage';
+import SearchPage from './Pages/SearchPage';
+import DetailsPage from './Pages/DetailsPage';
 // Components:
 // CSS:
 // Types, interfaces and enumns:
 
+// Routing:
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/search',
+        element: <SearchPage />,
+      },
+      {
+        path: '/packages/:name',
+        element: <DetailsPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
   // JSX:
-  return <div>Proof of life ...</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
