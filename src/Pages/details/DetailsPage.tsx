6c13001d68ccd9e59b79520e0bce9c4aa4b44eb6
Alex S <habahabahaba@outlook.com>
2024-09-17 06:36:56 +0400

    Add render template to the DetailsPage.tsx.

--- src/Pages/details/DetailsPage.tsx
+++ src/Pages/details/DetailsPage.tsx
@@ -9,14 +9,40 @@ import { useLoaderData } from 'react-router-dom';
 // CSS:
 // Types, interfaces and enumns:
 import type { FC } from 'react';
-// import type { PackageDetails } from '../../api/types/index.types';
 import type { DetailsLoaderReturn } from './detailsLoader';
 
 const DetailsPage: FC = () => {
-  const { packageDetails } = useLoaderData() as DetailsLoaderReturn;
-
+  const {
+    packageDetails: { name, description, license, author, latest },
+  } = useLoaderData() as DetailsLoaderReturn;
+  console.log(useLoaderData());
   // JSX:
-  return <div>{JSON.stringify(packageDetails)}</div>;
+  return (
+    <div className='space-y-4'>
+      <h1 className='text-3xl font-bold my-4'>{name}</h1>
+
+      {latest ? (
+        <h3 className='text-lg font-bold'>{`Latest version: ${latest}`}</h3>
+      ) : null}
+
+      <div>
+        <h3 className='text-lg font-bold'>Description:</h3>
+        <div className='p-3 bg-gray-200 rounded'>{description}</div>
+      </div>
+      {license ? (
+        <div>
+          <h3 className='text-lg font-bold'>License:</h3>
+          <div className='p-3 bg-gray-200 rounded'>{license}</div>
+        </div>
+      ) : null}
+      {author ? (
+        <div>
+          <h3 className='text-lg font-bold'>Author:</h3>
+          <div className='p-3 bg-gray-200 rounded'>{author.name}</div>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default DetailsPage;
