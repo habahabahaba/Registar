5dfb63ce7866227e33c9409bb0c029338f620864
Alex S <habahabahaba@outlook.com>
2024-09-21 06:05:06 +0400

    Change '%2f' to '%2F' in encodePath decodePath util functions.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,13 @@
 export function encodePath(path: string): string {
   while (path.includes('/')) {
-    path = path.replace('/', '%2f');
+    path = path.replace('/', '%2F');
   }
   return path;
 }
 
 export function decodePath(path: string): string {
-  while (path.includes('%2f')) {
-    path = path.replace('%2f', '/');
+  while (path.includes('%2F')) {
+    path = path.replace('%2F', '/');
   }
   return path;
 }
